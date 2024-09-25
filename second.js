efd6cbc2d7f22f193c267681421b858eb8ea9943
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-25 23:38:15 +0530

    adding file

---
+++ second.js
@@ -0,0 +1,9 @@
+/*
+  What is x?
+  Let's practice what we've just learned. Here you have to read code.
+
+  Exercise
+  Which value does x have after execution of the following code?
+  let x = 'Geeta';
+*/
+"Geeta"// here x will contain Geeta because x variable is intialized with the value as "Geeta"
