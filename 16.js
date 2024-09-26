9ae44659510565357238e42378004637fc54be3b
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-26 13:01:25 +0530

    adding file

---
+++ 16.js
@@ -0,0 +1,15 @@
+/*
+  Logging variables
+  Now let's log variables:
+  let scientist = 'Ken Thompson';
+  console.log(scientist);
+  The variable scientist is logged. The console outputs 'Ken Thompson'.
+  Exercise
+  Write a function log, that takes a parameter and logs this parameter.
+
+  Example: log('Ken Thompson') should log 'Ken Thompson'.
+*/
+function log(param){
+  console.log(param);
+}
+let res = log("Himanshu");
