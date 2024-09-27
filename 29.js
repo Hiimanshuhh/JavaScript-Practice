76dde8e338b4aedac6c7d4b558626a6eb54c7bf6
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-27 15:18:22 +0530

    adding file

---
+++ 29.js
@@ -0,0 +1,19 @@
+/*
+  Increment
+  In programming, you often want to increase or decrease a counter by one. This can easily be done with the increment or decrement 
+  operator.
+  let x = 1;
+  x++;
+  let y = 10;
+  y--;
+  x++ is the same as x = x + 1 and y-- is the same as y = y - 1. After executing the example code, x has the value 2 and y has the 
+  value 9.
+
+  Exercise
+  Which value does x have after execution of the following code?
+  let x = 3;
+  x++;
+  x = x * 2;
+  x--;
+*/
+7
