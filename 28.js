e8dac035070c8f6ebb1dc6ef115fc092c7a3e298
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-27 14:51:40 +0530

    adding file

---
+++ 28.js
@@ -0,0 +1,25 @@
+/*
+  Numbers
+  Numbers are represented by simple numerals. They can have a decimal point and a minus sign.
+  let x1 = 1;
+  let x2 = 1.0;
+  let x3 = 3.14;
+  let x4 = -1;
+  1 and 1.0 are the same number. You can calculate with numbers. The four basic arithmetics adding, subtracting, multiplying and dividing are represented by + - * and /.
+  let x1 = 6;
+  let x2 = 2;
+  let x3 = x1 + x2;
+  let x4 = x1 - x2;
+  let x5 = x1 * x2;
+  let x6 = x1 / x2;
+  The variables x3 to x6 thus have the values 8, 4, 12 and 3.
+
+  Exercise
+  Write a function add that takes two numbers and returns their sum.
+
+  Example: add(1, 2) should return 3.
+*/
+function add(num1, num2){
+  return num1+num2;
+  }
+  add(1,2);
