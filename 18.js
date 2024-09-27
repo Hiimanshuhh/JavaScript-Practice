3def63835a388260360f559c6da7e87da392de60
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-27 09:59:58 +0530

    updating

--- 18.js
+++ 18.js
@@ -1,7 +1,4 @@
 /*
-  Silent Teacher
-  A wonderful learn to code app is SILENT TEACHER. It's a minimalist game where beginners can discover what coding is all about and the key concepts behind code, with no explanations given. Through several series of questions, the player is led to guess the rules of this language and learn from his own mistakes. Since SILENT TEACHER uses JavaScript, it is an excellent supplement to JS Hero. More about the app can be found at Toxicode.
-
   Here the concatenation of strings shall be practiced again.
 
   Exercise
