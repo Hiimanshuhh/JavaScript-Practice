a495f09914482b422530b7086dcf76e4be95b6c2
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-27 15:29:56 +0530

    adding file

---
+++ 30.js
@@ -0,0 +1,17 @@
+/*
+  Fahrenheit
+
+  Exercise
+
+  Write a function toFahrenheit that converts a temperature from Celsius to Fahrenheit.
+
+  Example: toFahrenheit(0) should return 32.
+  + Hint
+  - Solution
+  function toFahrenheit(celsius) {
+    return 1.8 * celsius + 32;
+  }
+*/
+function toFahrenheit(celsius) {
+  return 1.8 * celsius + 32;
+}
